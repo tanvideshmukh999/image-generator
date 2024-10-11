6101afba1d4ff2262485c2e069b77a2c2e105fdc
Tanvi Deshmukh <tanvideshmukh999@gmail.com>
2024-10-11 18:59:40 +0530

    Added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [inputText,setInputText]=useState("");
+  const [imagelink,setImageLink]=useState("");
+  const API_TOKEN="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";
+
+  const fetchData=async()=>{
+    try{
+      const response =await axios.post(
+          'https://api-inference.huggingface.co/models/prompthero/openjourney-v4',
+          {
+            inputs: inputText,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${API_TOKEN}`,
+            },
+            responseType: 'blob', 
+          }
+        )
+        console.log(response.data);
+        const imageUrl = URL.createObjectURL(response.data);
+        console.log(imageUrl);
+        setImageLink(imageUrl);
+    
+    }
+    catch(e){
+      console.log("error occurs");
+    }
+  }
+
+
+  useEffect(()=>{
+    fetchData();
+  },[inputText])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Generator</h1>
+      
+     <input type="text"  value={inputText} onChange={(e)=>{
+      setInputText(e.target.value);
+
+     }}/>
+   <button>Generate</button>
+
+     <br/>
+     <img src={imagelink} alt=""/>
     </div>
   );
 }
